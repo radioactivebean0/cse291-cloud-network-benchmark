d22950df194fad1a47e22d977eaaff57612fefab
Ryan <ryan.h.nishimoto@gmail.com>
2022-11-14 20:40:00 -0800

    add continuous and limitless base patterns

--- client/__main__.py
+++ client/__main__.py
@@ -7,39 +7,25 @@ from rich.progress import Progress, SpinnerColumn, TextColumn
 
 app = typer.Typer(help="Benchmark runner for network benchmarks.")
 
-# we may need to add more parameters to smoothen out transmission if we
-# go to higher bitrates or even use multiprocessing.
 
 # destination is string of IP
 # bandwidth is in Mbps for now, int values, default = 1Mbps
-# isUDP defines if UDP or TCP test, default is TCP test
-# bidir is an arg to do tests in both directions, default is false
-def iperfContinuous(destination, time = 10, bandwidth = 1, isUDP = False, bidir = False):
+def iperf_continuous(destination, time = 10, bandwidth = 1):
     cmd = f"iperf3 -J -c {destination} -b {bandwidth}M -t {time}"
-    if isUDP:
-        cmd = cmd + " -u"
-    if bidir:
-        cmd = cmd + " -d"
     return cmd
-'''
-def iperfBurst(destination, burst_size, isUDP = False, bidir = False):
-    cmd = f"iperf3 -J -c {destination} -b 1280K/{burst_size} --pacing-timer 1000000"
-    if isUDP:
-        cmd = cmd + " -u"
-    if bidir:
-        cmd = cmd + " -d"
+
+# perform iperf with some amount of time with unlimited bandwidth
+def iperf_limitless(destination, time = 10):
+    cmd = f"iperf3 -J -c {destination} -b 0 -t {time}"
     return cmd
-'''
 
 def write_result(json: str):
     file_name = f"{datetime.now().isoformat()}.json"
     with open("results/" + file_name, "w") as f:
         f.write(json)
 
-
-@app.command()
-def client(destination: str, perf_arguments: str = ""):
-    command = iperfBurst(destination, 2)
+def bandwidth_test(destination):
+    command = iperf_limitless(destination)
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
     with Progress(
@@ -53,6 +39,10 @@ def client(destination: str, perf_arguments: str = ""):
         write_result(proc.stdout.read().decode("utf-8"))
         progress.update(proc_iperf, completed=1)
 
+
+@app.command()
+def client(destination: str, perf_arguments: str = ""):
+    bandwidth_test(destination)
     typer.echo("Test done")
 
 
