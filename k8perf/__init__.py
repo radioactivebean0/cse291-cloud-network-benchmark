de2f5ab8cd036cbfddaacab001c4d514601ccd91
Anders Aaen Springborg <anders.springborg@askcody.com>
2022-11-28 15:24:59 -0500

    rich logger

---
+++ k8perf/__init__.py
@@ -0,0 +1,12 @@
+import setuptools
+import logging
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
+log = logging.getLogger("rich")
+if __name__ == "__main__":
+    setuptools.setup(name="rich")
