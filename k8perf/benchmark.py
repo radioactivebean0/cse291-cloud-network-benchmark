a87aa76e9756a7028e77353737600793ce3c95d0
Anders Aaen Springborg <anders.springborg@askcody.com>
2022-12-09 15:37:08 -0800

    add locust benchmark

--- k8perf/benchmark.py
+++ k8perf/benchmark.py
@@ -3,31 +3,48 @@ from typing import Dict, List
 
 import pandas
 
-from k8perf.benchmarks import IPerfBenchmark
+from k8perf.benchmarks import IPerfBenchmark, LocustBenchmark
 
 
 class BenchmarkResult:
-    client_node: str
-    server_node: str
-    mbps: float
-    cpu_host: float
-    cpu_client: float
-    mean_rtt: float
-    retransmits: int
+    client_node: str = ""
+    server_node: str = ""
+    mbps: float = 0.0
+    cpu_server: float = 0.0
+    cpu_client: float = 0.0
+    mean_rtt: float = 0.0
+    retransmits: int = 0
+
+    httpMinResponseTime: int
+    httpMaxResponseTime: int
+    httpMedianResponseTime: int
+    httpAverageResponseTime: int
 
     def __init__(self, client_node: str, server_node: str):
         self.client_node = client_node
         self.server_node = server_node
 
     def __repr__(self):
-        return f"Mbps: {self.mbps}, cpu_host: {self.cpu_host}, cpu_client: {self.cpu_client}, mean_rtt: {self.mean_rtt}, retransmits: {self.retransmits}"
+        return f"Mbps: {self.mbps}, cpu_host: {self.cpu_server}, cpu_client: {self.cpu_client}, mean_rtt: {self.mean_rtt}, retransmits: {self.retransmits}"
 
     def add_iperf3(self, iperf3_json: Dict):
-        self.mbps = iperf3_json["end"]["sum_received"]["bits_per_second"] * 1e6
-        self.cpu_host = float(iperf3_json["end"]["cpu_utilization_percent"]["host_total"])
+        self.mbps = iperf3_json["end"]["sum_received"]["bits_per_second"] / 1e6
+        self.cpu_server = float(iperf3_json["end"]["cpu_utilization_percent"]["remote_total"])
         self.cpu_client = float(iperf3_json["end"]["cpu_utilization_percent"]["host_total"])
         self.mean_rtt = float(iperf3_json["end"]["streams"][0]["sender"]["mean_rtt"])
         self.retransmits = int(iperf3_json["end"]["streams"][0]["sender"]["retransmits"])
+
+    def add_locust(self, locust_benchmark: Dict):
+        get_request = locust_benchmark[0]
+        num_request = get_request["num_requests"]
+        average_response_time = 0
+        for time, amount in get_request["response_times"].items():
+            average_response_time += int(time) * amount
+        average_response_time /= num_request
+
+        self.httpMinResponseTime = get_request["min_response_time"]
+        self.httpMaxResponseTime = get_request["max_response_time"]
+        self.httpAverageResponseTime = average_response_time
 
 
 class BenchmarkResults:
@@ -40,10 +57,14 @@ class BenchmarkResults:
                 "server_node": benchmark_run.server_node,
                 "client_node": benchmark_run.client_node,
                 "mbps": benchmark_run.mbps,
-                "cpu_host": benchmark_run.cpu_host,
+                "cpu_host": benchmark_run.cpu_server,
                 "cpu_client": benchmark_run.cpu_client,
                 "mean_rtt": benchmark_run.mean_rtt,
-                "retransmits": benchmark_run.retransmits
+                "retransmits": benchmark_run.retransmits,
+                "httpMinResponseTime": benchmark_run.httpMinResponseTime,
+                "httpMaxResponseTime": benchmark_run.httpMaxResponseTime,
+                "httpAverageResponseTime": benchmark_run.httpAverageResponseTime,
+
             }, ignore_index=True)
 
     def __str__(self):
@@ -67,13 +88,18 @@ class BenchmarkRunner:
             for client_node in self.client_nodes:
                 logging.info("Running benchmark server %s: client: %s", client_node, server_node)
                 benchmark = BenchmarkResult(client_node, server_node)
+
                 iperfBenchmarkResult = IPerfBenchmark(client_node, server_node).run()
                 benchmark.add_iperf3(iperfBenchmarkResult)
+
+                locust_benchmark = LocustBenchmark(client_node, server_node).run()
+                benchmark.add_locust(locust_benchmark)
 
                 self.benchmark_results.add_result(benchmark)
 
     def cleanup(self):
         IPerfBenchmark("", "").cleanup()
+        LocustBenchmark("", "").cleanup()
 
     def json(self):
         return self.benchmark_results.json()
--- k8perf/benchmarks.py
+++ k8perf/benchmarks.py
@@ -1,7 +1,9 @@
 import json
+from dataclasses import dataclass
 from logging import info, debug
 from os import path
 from time import sleep
+from typing import Dict
 
 import typer
 
@@ -16,8 +18,72 @@ class LocustBenchmark:
         self.client_node = client_node
         self.server_node = server_node
         self.k8 = KubernetesIntegration(namespace="default")
-        self.server_resources = yaml.load_to_dicts("bandwidth/iperf3-server.yaml")
-        self.client_resources = yaml.load_to_dicts("bandwidth/iperf3-client.yaml")
+        self.server_resources = yaml.load_to_dicts("benchmarks/locust/server-locust.yaml")
+        self.client_resources = yaml.load_to_dicts("benchmarks/locust/client-locust.yaml")
+
+    def run(self) -> dict:
+        """Run the iperf3 benchmark and return the results as a dict, from the json parsed output"""
+        try:
+            benchmark = self.locust_benchmark()
+            self.cleanup()
+            return benchmark
+        except KeyboardInterrupt as e:
+            info("Keyboard interrupt, cleaning up")
+            self.cleanup()
+            raise e
+
+
+    def locust_benchmark(self):
+        debug("Running iperf3 benchmark on Kubernetes")
+        self.deploy_server()
+        return self.deploy_client()
+
+    def deploy_server(self):
+        deployment, service = self.server_resources
+        nodeSelector = {"kubernetes.io/hostname": self.server_node}
+        deployment["spec"]["template"]["spec"]["nodeSelector"] = nodeSelector
+        self.cleanup()
+        # Create deployment
+        # self.wait_for_resource(service, "Service")
+        info("Creating server deployment")
+        self.k8.create_from_dict(deployment)
+        info("Creating server service")
+        self.k8.create_from_dict(service)
+
+        info("Waiting: Server not ready")
+        self.k8.wait_for_resource(deployment, "Deployment")
+        nginx_locust_server = {"meta": {"name": "locust-server"}}
+        self.k8.wait_for_resource(nginx_locust_server, "Service")
+        sleep(1)
+
+    def deploy_client(self):
+        job = self.client_resources[0]
+        nodeSelector = {"kubernetes.io/hostname": self.client_node}
+        job["spec"]["template"]["spec"]["nodeSelector"] = nodeSelector
+        if self.k8.exists_in_kubernetes(job, "Job"):
+            self.k8.delete_job(job)
+            debug("Job already exists, deleting it")
+        # Create job
+        sleep(2)
+        info("Creating client benchmark job")
+        self.k8.create_from_dict(job)
+        debug(f"Job `{job['metadata']['name']}` created.")
+
+        return self.k8.get_job_logs(job)
+
+    def cleanup(self):
+        deployment, service = self.server_resources
+        if self.k8.exists_in_kubernetes(deployment, "Deployment"):
+            self.k8.delete_deployment(deployment)
+            self.k8.wait_for_deletion(deployment, "Deployment")
+        if self.k8.exists_in_kubernetes(service, "Service"):
+            self.k8.delete_service(service)
+            self.k8.wait_for_deletion(service, "Service")
+
+        job = self.client_resources[0]
+        if self.k8.exists_in_kubernetes(job, "Job"):
+            self.k8.delete_job(job)
+            self.k8.wait_for_deletion(job, "Job")
 
 
 class IPerfBenchmark:
@@ -37,6 +103,8 @@ class IPerfBenchmark:
             info("Keyboard interrupt, cleaning up")
             self.cleanup()
             raise e
+        else:
+            self.cleanup()
 
     def iperf3_benchmark_kubernetes(self, client_node, server_node):
 
